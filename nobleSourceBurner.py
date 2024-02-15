2af7f04d47a1312d6b7ce2971b6b7972a466c8ba
vimystic <122659254+vimystic@users.noreply.github.com>
2024-02-15 03:56:19 -0700

    update

--- nobleSourceBurner.py
+++ nobleSourceBurner.py
@@ -2,7 +2,6 @@ from web3 import Web3
 import bech32
 
 def address_to_bech32(address, tag):
-    print(address)
     if address == "":
         return ""
     address_bytes = bytes.fromhex(address)
@@ -22,12 +21,29 @@ def process_transaction(tx_hash, tag='noble'):
     bech32_address = address_to_bech32(address_hex_str, tag)
     return bech32_address
 
-alchemy_url = "hidden"
+alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/2PZMX2BG8IFkT_l_923CtNPrXIIbtlxr"
 w3 = Web3(Web3.HTTPProvider(alchemy_url))
 
 # Check if web3 is successfully connected
-print("Connected to Ethereum network:", w3.is_connected())
+# print("Connected to Ethereum network:", w3.is_connected())
 
-# TODO : Import tx's from csv file and store in a list.
-tx_hash = '0x1421274d360745b99a922b687e4516f7589e1eb999ddec9239daadf8a36f60cd'
-print("Bech32 address:", process_transaction(tx_hash))
+# Function to read transaction hashes from a CSV file
+def read_tx_hashes_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        data = file.read()
+        tx_hashes = data.split(',') 
+    return tx_hashes
+
+# File paths
+file_paths = [
+    '0xEF1a3C293875b8240F20d0Bbbb2461695Cd1E76d.csv',
+    '0xf3cc88ff74833abc6c04ba39c62ea608a138eb3c.csv'
+]
+
+tx_hashes = []
+for path in file_paths:
+    tx_hashes.extend(read_tx_hashes_from_csv(path))
+
+for tx_hash in tx_hashes:
+    bech32_address = process_transaction(tx_hash.strip()) 
+    print(f" {tx_hash}: {bech32_address}")
