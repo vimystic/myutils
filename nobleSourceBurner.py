de63751558689d6f7d147cb87c374c2b321e67b3
vimystic <122659254+vimystic@users.noreply.github.com>
2024-02-20 03:30:35 -0700

    Getting the source address now

--- nobleSourceBurner.py
+++ nobleSourceBurner.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 import bech32
+import json
 
 def address_to_bech32(address, tag):
     if address == "":
@@ -14,18 +15,16 @@ def address_to_bech32(address, tag):
 def process_transaction(tx_hash, tag='noble'):
     tx = w3.eth.get_transaction(tx_hash)
     input_data = tx.input
-    hex_input_data = input_data.hex() if isinstance(input_data, bytes) else input_data
-    if len(hex_input_data) < (133*2):
-        return ""
-    address_hex_str = hex_input_data[133*2:153*2]  
+    address_hex_str = decode_function_input(contract, input_data) 
     bech32_address = address_to_bech32(address_hex_str, tag)
     return bech32_address
 
-alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/2PZMX2BG8IFkT_l_923CtNPrXIIbtlxr"
-w3 = Web3(Web3.HTTPProvider(alchemy_url))
-
-# Check if web3 is successfully connected
-# print("Connected to Ethereum network:", w3.is_connected())
+def decode_function_input(contract, input_data):
+    if input_data == '0x':
+        return 'No data', {}
+    func_obj, func_params = contract.decode_function_input(input_data)
+    #print(f"Function: {func_obj.fn_name}, Params: {func_params}")
+    return (func_params['message']).hex()[-40:]
 
 # Function to read transaction hashes from a CSV file
 def read_tx_hashes_from_csv(file_path):
@@ -33,6 +32,19 @@ def read_tx_hashes_from_csv(file_path):
         data = file.read()
         tx_hashes = data.split(',') 
     return tx_hashes
+
+
+alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/2PZMX2BG8IFkT_l_923CtNPrXIIbtlxr"
+w3 = Web3(Web3.HTTPProvider(alchemy_url))
+with open('abi.json', 'r') as abi_definition:
+    contract_abi = json.load(abi_definition)
+contract_address = "0x0a992d191DEeC32aFe36203Ad87D7d289a738F81"
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+
+
+# Check if web3 is successfully connected
+# print("Connected to Ethereum network:", w3.is_connected())
 
 # File paths
 file_paths = [
