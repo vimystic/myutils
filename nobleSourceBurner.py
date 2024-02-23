7d11843452f6f2c51cab3440a1593a0574b2b9e0
vimystic <122659254+vimystic@users.noreply.github.com>
2024-02-23 01:22:30 -0700

    Add nonces to csv

--- nobleSourceBurner.py
+++ nobleSourceBurner.py
@@ -19,37 +19,36 @@ def noble_to_dydx(noble_address):
     hrp, data = bech32.bech32_decode(noble_address)
     if data is None:
         return "NA"
-    
     converted_bits = bech32.convertbits(data, 5, 8, False)
     if converted_bits is None:
         return "NA"
-    
     hex_address = bytes(converted_bits).hex()
     dydx_address = address_to_bech32(hex_address, "dydx")
-    
     return dydx_address
 
 def process_transaction(tx_hash, tag='noble'):
     tx = w3.eth.get_transaction(tx_hash)
     input_data = tx.input
-    address_hex_str = decode_function_input(contract, input_data)
+    address_hex_str, nonce = decode_function_input(contract, input_data)
     if address_hex_str == 'Error decoding input':
-        return 'Error decoding transaction input'
+        return 'Error decoding transaction input', 'Error decoding nonce'
     else:
         bech32_address = address_to_bech32(address_hex_str, tag)
-        return bech32_address
-
+        return bech32_address, nonce
 
 def decode_function_input(contract, input_data):
     if input_data == '0x':
         return 'No data', {}
     try:
         func_obj, func_params = contract.decode_function_input(input_data)
-        return (func_params['message']).hex()[-40:]
+        message_hex = (func_params['message']).hex()
+        last_40_chars = message_hex[-40:]
+        nonce_included_hex = message_hex[24:40]  # Hexadecimal string of the nonce
+        nonce_included = int(nonce_included_hex, 16)  # Convert hex to int
+        return last_40_chars, nonce_included
     except ValueError as e:
         print(f"Error decoding function input: {e}")
-        return 'Error decoding input'
-
+        return 'Error decoding input', {}
 
 def process_tx_hashes_and_update_csv(file_path):
     updated_rows = []
@@ -57,17 +56,16 @@ def process_tx_hashes_and_update_csv(file_path):
     with open(file_path, mode='r', newline='') as file:
         reader = csv.reader(file)
         header = next(reader)
-        updated_rows.append(header + ['Noble Address', 'Dydx Address'])
+        updated_rows.append(header + ['Noble Address', 'Dydx Address', 'Nonce'])
         for row in reader:
             tx_hash = row[0].strip()
-            noble_address = process_transaction(tx_hash)
+            noble_address, nonce = process_transaction(tx_hash)
             dydx_address = noble_to_dydx(noble_address)
-            updated_rows.append(row + [noble_address, dydx_address])
+            updated_rows.append(row + [noble_address, dydx_address, nonce])
     
     with open(file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(updated_rows)
-
 
 alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/2PZMX2BG8IFkT_l_923CtNPrXIIbtlxr"
 w3 = Web3(Web3.HTTPProvider(alchemy_url))
@@ -76,12 +74,7 @@ with open('abi.json', 'r') as abi_definition:
 contract_address = "0x0a992d191DEeC32aFe36203Ad87D7d289a738F81"
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
-
-
-# Check if web3 is successfully connected
-# print("Connected to Ethereum network:", w3.is_connected())
-
-# File paths . These are csv files from etherscan.io
+# File paths. These are csv files from etherscan.io
 file_paths = [
     '0xEF1a3C293875b8240F20d0Bbbb2461695Cd1E76d.csv',
     '0xf3cc88ff74833abc6c04ba39c62ea608a138eb3c.csv'
@@ -89,5 +82,3 @@ file_paths = [
 
 for path in file_paths:
     process_tx_hashes_and_update_csv(path)
-    
-
